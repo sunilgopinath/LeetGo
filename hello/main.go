62441690555cb700d0d71ff568cb3cc079994bc3
sunilffx <sunil.x.gopinath@gmail.com>
2017-11-03 23:47:16 +1100

    adding initial directories

---
+++ hello/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sunilgopinath/stringutil"
+	"github.com/sunilgopinath/twosum"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(twosum.twoSum([]int{1, 2, 3, 4}, 17))
+}
---
+++ twosum/twosum.go
@@ -0,0 +1,6 @@
+package twosum
+
+//TwoSum finds a pair of numbers which adds to the target
+func TwoSum(nums []int, target int) []int {
+	return []int{1}
+}
