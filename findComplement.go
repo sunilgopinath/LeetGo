f827d35dc822968774a4a2ac838f307bbb6308f5
Sunil Gopinath <sxg282@gmail.com>
2018-02-16 16:27:21 +1100

    looking for complement in Go

---
+++ findComplement.go
@@ -0,0 +1,31 @@
+package complement
+
+func findComplement(num int) int {
+    return toDecimal(complement(toBinary(num)))
+}
+
+func toBinary(n int) string {
+	s := strconv.FormatInt(int64(n), 2)
+	return s
+}
+
+func toDecimal(s string) int {
+	var n float64
+	for i := len(s) - 1; i >= 0; i-- {
+		t, _ := strconv.Atoi(string(s[i]))
+		n += float64(t) * math.Pow(2, float64(len(s)-1-i))
+	}
+	return int(n)
+}
+
+func complement(s string) string {
+	var res string
+	for _, r := range []rune(s) {
+		if r-'0' == 1 {
+			res += "0"
+		} else {
+			res += "1"
+		}
+	}
+	return res
+}
